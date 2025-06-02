eeb2d567ce2616bab0e2100acb5bb8e8fdf264af
Jamie Donovan <jamie.donovan97@gmail.com>
2025-06-02 15:09:58 +1000

    Yes

--- ICMInterop/2DZones/obj/Release/net8.0/Civil3D.ICMInterop.2DZones.AssemblyInfo.cs
+++ ICMInterop/2DZones/obj/Release/net8.0/Civil3D.ICMInterop.2DZones.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Civil3D.ICMInterop.2DZones")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+678770519e7e432e6ee4158134265c888111495e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Civil3D.ICMInterop.2DZones")]
